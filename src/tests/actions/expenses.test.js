6fa165348fdb282cbd5b32e1c85c87d9a4634bfa
Nicholas de Waal <nick.a.dewaal@gmail.com>
2020-12-01 07:57:02 +0900

    workong on firebase redux test suites

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,4 +1,5 @@
 import { addExpense, editExpense, removeExpense } from "../../actions/expenses";
+import expenses from "../fixtures/expenses";
 
 test("should setup remove expense action object", () => {
   const action = removeExpense({ id: "123abc" });
@@ -19,34 +20,32 @@ test("should setup edit expense action object", () => {
   });
 });
 
-test("should setup addExpense action object", () => {
-  const expenseData = {
-    description: "Rent",
-    amount: 1000,
-    createdAt: 1000,
-    note: "This was last months rent",
-  };
-
-  const action = addExpense(expenseData);
+test("should setup addExpense action object with provided values", () => {
+  const action = addExpense(expenses[2]);
   expect(action).toEqual({
     type: "ADD_EXPENSE",
-    expense: {
-      ...expenseData,
-      id: expect.any(String),
-    },
+    expense: expenses[2],
   });
 });
 
-test("should setup addExpense action object with default values", () => {
-  const action = addExpense();
-  expect(action).toEqual({
-    type: "ADD_EXPENSE",
-    expense: {
-      id: expect.any(String),
-      description: "",
-      note: "",
-      amount: 0,
-      createdAt: 0,
-    },
-  });
-});
+test('should add expense to database and store', () => {
+
+})
+
+test('should add expense with defaults to database and store', () => {
+  
+})
+
+// test("should setup addExpense action object with default values", () => {
+//   const action = addExpense();
+//   expect(action).toEqual({
+//     type: "ADD_EXPENSE",
+//     expense: {
+//       id: expect.any(String),
+//       description: "",
+//       note: "",
+//       amount: 0,
+//       createdAt: 0,
+//     },
+//   });
+// });
